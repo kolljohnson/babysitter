419a2af87fbff7b38ac0e26d06000e12c24847db
Koll Johnson <koll-johnson@openmailbox.org>
2017-03-29 22:32:57 -0500

    Add ValidStartTime Test

--- test/test.js
+++ test/test.js
@@ -1,1 +1,8 @@
 const expect = require('chai').expect();
+const babysitter = require('../index');
+
+describe('Babysitter', () => {
+    it('should only allow start time to be after 5:00PM', () => {
+	expect(babysitter.calculateCharge('4:00 PM')).to.equal('Error: Start date must be at or after 5:00PM.');
+    });
+});
