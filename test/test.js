69997f8b9d38c5b681eef48c2c7dbbfd5f2a5942
Koll Johnson <koll-johnson@openmailbox.org>
2017-03-30 21:19:24 -0500

    Refactor

--- test/test.js
+++ test/test.js
@@ -3,22 +3,23 @@ const babysitter = require('../index');
 
 describe('Babysitter', () => {
     describe('Start Time', () => {
+	let endTime = new Date('March 31, 2017 21:00:00');
 	it('should only allow start time to be after 5:00PM', () => {
-	    let startDate = new Date('March 31, 2017 16:00:00');
-	    expect(babysitter.calculateCharge(startDate)).to.equal('Error: Start date must be at or after 5:00PM.');
+	    let startTime = new Date('March 31, 2017 16:00:00');
+	    expect(babysitter.calculateCharge(startTime, endTime)).to.equal('Error: Start date must be at or after 5:00PM.');
 	});
 
 	it('should accept a valid start time', () => {
-	    let startDate = new Date('March 31, 2017 17:00:00');
-	    expect(babysitter.calculateCharge(startDate)).to.equal(true);
+	    let startTime = new Date('March 31, 2017 17:00:00');
+	    expect(babysitter.calculateCharge(startTime, endTime)).to.equal(true);
 	});
     });
 
     describe('End Time', () => {
 	it('should accept an end time', () => {
-	    let startDate = new Date('March 31, 2017 17:00:00');
-	    let endDate = new Date('March 31, 2017 21:00:00');
-	    expect(babysitter.calculateCharge(startDate, endDate)).to.equal(true);
+	    let startTime = new Date('March 31, 2017 17:00:00');
+	    let endTime = new Date('March 31, 2017 21:00:00');
+	    expect(babysitter.calculateCharge(startTime, endTime)).to.equal(true);
 	});
     });
 });
