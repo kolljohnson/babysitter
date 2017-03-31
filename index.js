2a936537523a47ece508dbb79d92f370350684d4
Koll Johnson <koll-johnson@openmailbox.org>
2017-03-30 21:28:44 -0500

    Implement ValidEndTime

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 function calculateCharge (startTime, endTime) {
     if(startTime.getHours() <= 16) {
 	return 'Error: Start date must be at or after 5:00PM.';
+    } else if(endTime.getHours() > 4 && endTime.getHours() < 17){
+	return 'Error: End time must be at or before 4:00AM.';
     } else {
 	return true;
     }
