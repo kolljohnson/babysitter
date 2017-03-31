e116add751719403c669c2773eb293fdea8023e0
Koll Johnson <koll-johnson@openmailbox.org>
2017-03-30 21:39:31 -0500

    Refactor

--- test/test.js
+++ test/test.js
@@ -5,6 +5,8 @@ describe('Babysitter', () => {
     describe('Start Time', () => {
 
 	let endTime = new Date('March 31, 2017 21:00:00');
+	let bedTime = new Date('March 31, 2017 21:00:00');
+
 
 	it('should only allow start time to be after 5:00PM', () => {
 	    let startTime = new Date('March 31, 2017 16:00:00');
@@ -20,7 +22,8 @@ describe('Babysitter', () => {
     describe('End Time', () => {
 
 	let startTime = new Date('March 31, 2017 17:00:00');
-
+	let bedTime = new Date('March 31, 2017 21:00:00');
+	
 	it('should accept an end time', () => {
 	    let endTime = new Date('March 31, 2017 21:00:00');
 	    expect(babysitter.calculateCharge(startTime, endTime)).to.equal(true);
