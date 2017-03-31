61d641da9754b73cb40e18966b0f69a517be328b
Koll Johnson <koll-johnson@openmailbox.org>
2017-03-30 21:13:23 -0500

    Implement AcceptEndTime Test

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 
-function calculateCharge (startTime) {
+function calculateCharge (startTime, endTime) {
     if(startTime.getHours() <= 16) {
 	return 'Error: Start date must be at or after 5:00PM.';
     } else {
