e53ea974d09a57696c88607329bdc730b844858b
Koll Johnson <koll-johnson@openmailbox.org>
2017-03-30 21:12:28 -0500

    Add AcceptEndTime Test

--- test/test.js
+++ test/test.js
@@ -13,4 +13,12 @@ describe('Babysitter', () => {
 	    expect(babysitter.calculateCharge(startDate)).to.equal(true);
 	});
     });
+
+    describe('End Time', () => {
+	it('should accept an end time', () => {
+	    let startDate = new Date('March 31, 2017 17:00:00');
+	    let endDate = new Date('March 31, 2017 21:00:00');
+	    expect(babysitter.calculateCharge(startDate, endDate)).to.equal(true);
+	});
+    });
 });
