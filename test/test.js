a5d8dd436fb811384bde793634185b36290f7b8d
Koll Johnson <koll-johnson@openmailbox.org>
2017-03-30 20:55:39 -0500

    Refactor

--- test/test.js
+++ test/test.js
@@ -2,13 +2,15 @@ const expect = require('chai').expect;
 const babysitter = require('../index');
 
 describe('Babysitter', () => {
-    it('should only allow start time to be after 5:00PM', () => {
-	let startDate = new Date('March 31, 2017 16:00:00');
-	expect(babysitter.calculateCharge(startDate)).to.equal('Error: Start date must be at or after 5:00PM.');
-    });
+    describe('Start Time', () => {
+	it('should only allow start time to be after 5:00PM', () => {
+	    let startDate = new Date('March 31, 2017 16:00:00');
+	    expect(babysitter.calculateCharge(startDate)).to.equal('Error: Start date must be at or after 5:00PM.');
+	});
 
-    it('should accept a valid start time', () => {
-	let startDate = new Date('March 31, 2017 17:00:00');
-	expect(babysitter.calculateCharge(startDate)).to.equal(true);
+	it('should accept a valid start time', () => {
+	    let startDate = new Date('March 31, 2017 17:00:00');
+	    expect(babysitter.calculateCharge(startDate)).to.equal(true);
+	});
     });
 });
