03d5d74694ee3e19078853c73fbb8b320d81b6b6
Koll Johnson <koll-johnson@openmailbox.org>
2017-03-30 22:12:06 -0500

    Refactor

--- test/test.js
+++ test/test.js
@@ -10,12 +10,12 @@ describe('Babysitter', () => {
 
 	it('should only allow start time to be after 5:00PM', () => {
 	    let startTime = new Date('March 31, 2017 16:00:00');
-	    expect(babysitter.calculateCharge(startTime, endTime)).to.equal('Error: Start time must be at or after 5:00PM.');
+	    expect(babysitter.calculateCharge(startTime, endTime, bedTime)).to.equal('Error: Start time must be at or after 5:00PM.');
 	});
 
 	it('should accept a valid start time', () => {
 	    let startTime = new Date('March 31, 2017 17:00:00');
-	    expect(babysitter.calculateCharge(startTime, endTime)).to.equal(0);
+	    expect(babysitter.calculateCharge(startTime, endTime, bedTime)).to.equal(48);
 	});
     });
 
@@ -26,7 +26,7 @@ describe('Babysitter', () => {
 	
 	it('should accept an end time', () => {
 	    let endTime = new Date('March 31, 2017 21:00:00');
-	    expect(babysitter.calculateCharge(startTime, endTime)).to.equal(0);
+	    expect(babysitter.calculateCharge(startTime, endTime, bedTime)).to.equal(48);
 	});
 
 	it('should only allow end time to be before or at 4:00AM', () => {
@@ -41,7 +41,7 @@ describe('Babysitter', () => {
 
 	it('should accept a bedtime', () => {
 	    let bedTime = new Date('March 31, 2017 21:00:00');
-	    expect(babysitter.calculateCharge(startTime, endTime, bedTime)).to.equal(0);
+	    expect(babysitter.calculateCharge(startTime, endTime, bedTime)).to.equal(48);
 	});
     });
 
@@ -52,7 +52,7 @@ describe('Babysitter', () => {
 	    let endTime = new Date('March 31, 2017 20:00:00');
 	    let bedTime = new Date('March 31, 2017 21:00:00');
 
-	    expect(babysitter.calculateCharge(startTime, endTime, bedTime)).to.equal(0);
+	    expect(babysitter.calculateCharge(startTime, endTime, bedTime)).to.equal(48);
 	});
 
 	it('should pay $12/hour from start-time to bedtime', () => {
