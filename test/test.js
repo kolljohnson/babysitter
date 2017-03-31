d7d1be4b9ca908d5ad6c2ef52a7e8de34a4f461b
Koll Johnson <koll-johnson@openmailbox.org>
2017-03-30 20:52:08 -0500

    Add AcceptValidStartTime Test

--- test/test.js
+++ test/test.js
@@ -6,4 +6,9 @@ describe('Babysitter', () => {
 	let startDate = new Date('March 31, 2017 16:00:00');
 	expect(babysitter.calculateCharge(startDate)).to.equal('Error: Start date must be at or after 5:00PM.');
     });
+
+    it('should accept a valid start time', () => {
+	let startDate = new Date('March 31, 2017 17:00:00');
+	expect(babysitter.calculateCharge(startDate)).to.equal(true);
+    });
 });
